7234c7c0c6d505809ba1348fa448be9654fee374
tphanson <tphanson@kambria.io>
2019-10-29 15:47:07 +0700

    line detection model

--- lane_detector.py
+++ lane_detector.py
@@ -1,24 +1,26 @@
 import cv2 as cv
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 
 
 def do_canny(frame):
     gray = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
-    blur = cv.GaussianBlur(gray, (5, 5), 0)
+    blur = cv.GaussianBlur(gray, (15, 15), 0)
     canny = cv.Canny(blur, 50, 150)
     return canny
 
 
-def do_segment(frame):
+def do_rectangle(frame):
     height = frame.shape[0]
+    width = frame.shape[1]
     polygons = np.array([
-        [(0, height), (800, height), (380, 290)]
+        [(0, height), (width, height),
+         (width, int(height/2)), (0, int(height/2))]
     ])
     mask = np.zeros_like(frame)
     cv.fillPoly(mask, polygons, 255)
-    segment = cv.bitwise_and(frame, mask)
-    return segment
+    rectangle = cv.bitwise_and(frame, mask)
+    return rectangle
 
 
 def calculate_lines(frame, lines):
@@ -33,17 +35,20 @@ def calculate_lines(frame, lines):
             left.append((slope, y_intercept))
         else:
             right.append((slope, y_intercept))
-    left_avg = np.average(left, axis=0)
-    right_avg = np.average(right, axis=0)
-    left_line = calculate_coordinates(frame, left_avg)
-    right_line = calculate_coordinates(frame, right_avg)
-    return np.array([left_line, right_line])
+    if(len(left) != 0):
+        left = np.average(left, axis=0)
+        left = calculate_coordinates(frame, left)
+    if(len(right) != 0):
+        right = np.average(right, axis=0)
+        right = calculate_coordinates(frame, right)
+    return np.array([left, right])
 
 
 def calculate_coordinates(frame, parameters):
+    height = frame.shape[0]
     slope, intercept = parameters
-    y1 = frame.shape[0]
-    y2 = int(y1 - 150)
+    y1 = height
+    y2 = int(height/2)
     x1 = int((y1 - intercept) / slope)
     x2 = int((y2 - intercept) / slope)
     return np.array([x1, y1, x2, y2])
@@ -51,30 +56,47 @@ def calculate_coordinates(frame, parameters):
 
 def visualize_lines(frame, lines):
     lines_visualize = np.zeros_like(frame)
-    if lines is not None:
-        for x1, y1, x2, y2 in lines:
+    for line in lines:
+        if len(line) != 0:
+            x1, y1, x2, y2 = line
             cv.line(lines_visualize, (x1, y1), (x2, y2), (0, 255, 0), 5)
     return lines_visualize
 
 
-CAP = cv.VideoCapture("data/input.mp4")
-while (CAP.isOpened()):
-    RET, FRAME = CAP.read()
-    CANNY = do_canny(FRAME)
-    # plt.imshow(CANNY)
-    # plt.show()
-    SEGMENT = do_segment(CANNY)
-    HOUGH = cv.HoughLinesP(SEGMENT, 2, np.pi / 180, 100,
-                           np.array([]), minLineLength=100, maxLineGap=50)
-    LINES = calculate_lines(FRAME, HOUGH)
-    print("===========")
-    print(LINES)
-    VISUAL_LINES = visualize_lines(FRAME, LINES)
-    OUTPUT = cv.addWeighted(FRAME, 0.9, VISUAL_LINES, 1, 1)
-    cv.imshow("ouput", OUTPUT)
-    if cv.waitKey(10) & 0xFF == ord('q'):
-        break
+def main():
+    cap = cv.VideoCapture("data/autocar.mp4")
+    while (cap.isOpened()):
+        ret, frame = cap.read()
+        canny = do_canny(frame)
+        # plt.imshow(canny)
+        # plt.show()
+
+        segment = do_rectangle(canny)
+        hough = cv.HoughLinesP(segment, 1, np.pi / 90, 50,
+                               np.array([]), minLineLength=100, maxLineGap=100)
+        # Test run
+        # test = np.array([])
+        # if(hough is not None):
+        #     for line in hough:
+        #         if(len(test) == 0):
+        #             test = line
+        #         test = np.append(test, line, axis=0)
+        # cvlines = visualize_lines(frame, test)
+
+        # Real run
+        lines = calculate_lines(frame, hough)
+        cvlines = visualize_lines(frame, lines)
+
+        # Visualize
+        output = cv.addWeighted(frame, 0.9, cvlines, 1, 1)
+        cv.imshow("Realtime", output)
+
+        if cv.waitKey(10) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv.destroyAllWindows()
 
 
-CAP.release()
-cv.destroyAllWindows()
+if __name__ == "__main__":
+    main()
